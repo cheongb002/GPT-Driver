30c6f3f122473b5bd655b17db2323dda9c5f3180
Brian Cheong <cheongb002@gmail.com>
2024-03-25 11:34:20 -0400

    Add new files and update .gitignore

--- gpt-driver/create_data.py
+++ gpt-driver/create_data.py
@@ -1,8 +1,24 @@
-import pickle
-import ndjson
 import json
-import tiktoken
-from prompt_message import system_message, generate_user_message, generate_assistant_message
+import os
+import pickle
+
+import tqdm
+from datasets import Dataset
+from prompt_message import (generate_assistant_message, generate_user_message,
+                            system_message)
+
+
+def format_conversation(row):
+    template = "<|im_start|>system\n{sys}<|im_end|>\n<|im_start|>user\n{q}<|im_end|>\n<|im_start|>assistant\n{a}<|im_end|>"
+
+    conversation = template.format(
+        sys=system_message,
+        q=row["user"],
+        a=row["assistant"],
+    )
+
+    return {"text": conversation}
+
 
 data = pickle.load(open('data/cached_nuscenes_info.pkl', 'rb'))
 split = json.load(open('data/split.json', 'r'))
@@ -12,7 +28,6 @@ val_tokens = split["val"]
 num_train_samples = len(train_tokens)
 train_ratio = 1
 
-encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 
 num_language_tokens = 0
 num_system_tokens = 0
@@ -22,39 +37,39 @@ num_assistant_tokens = 0
 traj_only = False
 
 train_messages = []
-for token_i, token in enumerate(train_tokens):
+for token_i, token in tqdm.tqdm(enumerate(train_tokens)):
     if token_i >= train_ratio * num_train_samples:
-        break 
+        break
     user_message = generate_user_message(data, token)
-    assitant_message = generate_assistant_message(data, token, traj_only=traj_only)
-    if len(assitant_message.split("\n")) > 6:
-        print()
-        print(token)
-        print(system_message)
-        print(user_message)
-        print(assitant_message)
-    num_language_tokens += len(encoding.encode(system_message))
-    num_system_tokens += len(encoding.encode(system_message))
-    num_language_tokens += len(encoding.encode(user_message))
-    num_user_tokens += len(encoding.encode(user_message))
-    num_language_tokens += len(encoding.encode(assitant_message))
-    num_assistant_tokens += len(encoding.encode(assitant_message))
+    assitant_message = generate_assistant_message(
+        data, token, traj_only=traj_only)
+    train_messages.append({
+        "user": user_message,
+        "assistant": assitant_message
+    })
+    # if len(assitant_message.split("\n")) > 6:
+    #     print()
+    #     print(token)
+    #     print(system_message)
+    #     print(user_message)
+    #     print(assitant_message)
 
+    # train_message = {"messages":
+    #     [
+    #         {"role": "system", "content": system_message},
+    #         {"role": "user", "content": user_message},
+    #         {"role": "assistant", "content": assitant_message}
+    #     ]
+    # }
+    # train_messages.append(train_message)
 
-    train_message = {"messages": 
-        [
-            {"role": "system", "content": system_message},
-            {"role": "user", "content": user_message}, 
-            {"role": "assistant", "content": assitant_message}
-        ]
-    }
-    train_messages.append(train_message)
-
-print("#### Cost Summarization ####")
-print(f"Number of system tokens: {num_system_tokens}")
-print(f"Number of user tokens: {num_user_tokens}")
-print(f"Number of assistant tokens: {num_assistant_tokens}")
-print(f"Number of total tokens: {num_language_tokens}")
-
-with open("data/train.json", "w") as f:
-    ndjson.dump(train_messages, f)
+# with open("data/train.json", "w") as f:
+#     ndjson.dump(train_messages, f)
+dataset = Dataset.from_list(train_messages)
+dataset = dataset.map(
+    format_conversation,
+    # remove all columns; only "text" will be left
+    remove_columns=dataset.column_names,
+    num_proc=os.cpu_count()  # multithreaded
+)
+dataset.save_to_disk("data/processed")
--- gpt-driver/finetune.py
+++ gpt-driver/finetune.py
@@ -1,20 +1,137 @@
-import openai
+import os
 
-openai.api_key = "" # insert your API key here
+import torch
+from datasets import load_from_disk
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from transformers import (AutoModelForCausalLM, AutoTokenizer,
+                          BitsAndBytesConfig, QuantoConfig, Trainer,
+                          TrainingArguments)
 
-response = openai.File.create(file=open("train.json", "r"), purpose='fine-tune')
-print(response)
-train_file_id = response["id"]
+dataset = load_from_disk("data/processed")
+print("dataset loaded")
 
-response = openai.FineTuningJob.create(training_file=train_file_id, model="gpt-3.5-turbo", hyperparameters={"n_epochs":1, })
-print(response)
-finetune_job_id = response["id"]
+modelpath = "mistralai/Mixtral-8x7B-v0.1"
+quantization_config = QuantoConfig(weights="int8")
+bnb_quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_quant_type="nf4",
+    llm_int8_enable_fp32_cpu_offload=True
+)
 
-# List 10 fine-tuning jobs
-openai.FineTuningJob.list(limit=10)
+# Load 4-bit quantized model
+model = AutoModelForCausalLM.from_pretrained(
+    modelpath,
+    device_map="auto",
+    # quantization_config=quantization_config,
+    quantization_config=bnb_quantization_config,
+    torch_dtype=torch.bfloat16,
+    offload_buffers=True,
+)
+print("model loaded")
 
-# Retrieve the state of a fine-tune
-openai.FineTuningJob.retrieve(finetune_job_id)
+# Load (slow) Tokenizer, fast tokenizer sometimes ignores added tokens
+tokenizer = AutoTokenizer.from_pretrained(modelpath, use_fast=False)
+print("tokenizer loaded")
 
-# List up to 10 events from a fine-tuning job
-openai.FineTuningJob.list_events(id=finetune_job_id, limit=10)
+# Add tokens <|im_start|> and <|im_end|>, latter is special eos token
+tokenizer.pad_token = "</s>"
+tokenizer.add_tokens(["<|im_start|>"])
+tokenizer.add_special_tokens(dict(eos_token="<|im_end|>"))
+model.resize_token_embeddings(len(tokenizer))
+model.config.eos_token_id = tokenizer.eos_token_id
+
+
+# Add LoRA adapters to model
+model = prepare_model_for_kbit_training(model)
+config = LoraConfig(
+    r=64,
+    lora_alpha=16,
+    target_modules=['q_proj', 'k_proj', 'down_proj',
+                    'v_proj', 'gate_proj', 'o_proj', 'up_proj'],
+    lora_dropout=0.1,
+    bias="none",
+    # needed because we added new tokens to tokenizer/model
+    modules_to_save=["lm_head", "embed_tokens"],
+    task_type="CAUSAL_LM"
+)
+model = get_peft_model(model, config)
+model.config.use_cache = False
+
+
+def tokenize(element):
+    return tokenizer(
+        element["text"],
+        truncation=True,
+        max_length=2048,
+        add_special_tokens=False,
+    )
+
+
+dataset_tokenized = dataset.map(
+    tokenize,
+    batched=True,
+    num_proc=os.cpu_count(),    # multithreaded
+    # don't need the strings anymore, we have tokens from here on
+    remove_columns=["text"]
+)
+print("dataset tokenized")
+
+# collate function - to transform list of dictionaries [ {input_ids: [123, ..]}, {.. ] to single batch dictionary { input_ids: [..], labels: [..], attention_mask: [..] }
+
+
+def collate(elements):
+    tokenlist = [e["input_ids"] for e in elements]
+    tokens_maxlen = max([len(t) for t in tokenlist])  # length of longest input
+
+    input_ids, labels, attention_masks = [], [], []
+    for tokens in tokenlist:
+        # how many pad tokens to add for this sample
+        pad_len = tokens_maxlen-len(tokens)
+
+        # pad input_ids with pad_token, labels with ignore_index (-100) and set attention_mask 1 where content, otherwise 0
+        input_ids.append(tokens + [tokenizer.pad_token_id]*pad_len)
+        labels.append(tokens + [-100]*pad_len)
+        attention_masks.append([1]*len(tokens) + [0]*pad_len)
+
+    batch = {
+        "input_ids": torch.tensor(input_ids),
+        "labels": torch.tensor(labels),
+        "attention_mask": torch.tensor(attention_masks)
+    }
+    return batch
+
+
+bs = 2        # batch size
+ga_steps = 1  # gradient acc. steps
+epochs = 5
+steps_per_epoch = len(dataset_tokenized["train"])//(bs*ga_steps)
+
+args = TrainingArguments(
+    output_dir="out",
+    per_device_train_batch_size=bs,
+    per_device_eval_batch_size=bs,
+    evaluation_strategy="steps",
+    logging_steps=1,
+    eval_steps=steps_per_epoch,  # eval and save once per epoch
+    save_steps=steps_per_epoch,
+    gradient_accumulation_steps=ga_steps,
+    num_train_epochs=epochs,
+    lr_scheduler_type="constant",
+    optim="paged_adamw_32bit",
+    learning_rate=0.0002,
+    group_by_length=True,
+    fp16=True,
+    ddp_find_unused_parameters=False,    # needed for training with accelerate
+)
+
+trainer = Trainer(
+    model=model,
+    tokenizer=tokenizer,
+    data_collator=collate,
+    train_dataset=dataset_tokenized["train"],
+    eval_dataset=dataset_tokenized["test"],
+    args=args,
+)
+
+trainer.train()
